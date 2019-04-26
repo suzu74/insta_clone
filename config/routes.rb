9b5866ae0f96be43652c63c3f8074703af439ed5
Tomo <>
2019-04-26 03:00:12 +0000

    Make a basic User model (including validates)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
+  get  '/signup',  to: 'users#new'
+
 end
