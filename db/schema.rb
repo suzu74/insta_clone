f60a0e30bbb77ee5f320fdb39ba81ea33486b810
Tomo <>
2019-04-29 08:43:26 +0000

    Add Relationship model]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190427231819) do
+ActiveRecord::Schema.define(version: 20190429075909) do
 
   create_table "images", force: :cascade do |t|
     t.text "content"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20190427231819) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_images_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_images_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
