8bddcea7c2be5129cc0915e45e8738bedd22da22
Tomo <>
2019-05-01 01:12:49 +0000

    Make 3column layout

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,6 +5,7 @@ class Image < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
+  validates :picture, presence: true
   
   private
 
